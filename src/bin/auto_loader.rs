9040027f0a3126847cc5d11db9f436f554fec4a4
eysho.dev <eysho.it@gmail.com>
2026-07-11 17:38:09 +0200

    fix: remove broken compile-time manifest include

--- src/bin/auto_loader.rs
+++ src/bin/auto_loader.rs
@@ -2,17 +2,16 @@ use anyhow::Result;
 use bitshit_auto_loader::ComponentRegistry;
 use std::path::PathBuf;
 
-const PACKAGE_MANIFEST: &str = include_str!("../package.json");
-
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("package.json");
-    let install_root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("components");
+    let repo_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let manifest_path = repo_root.join("package.json");
+    let install_root = repo_root.join("components");
 
     let registry = ComponentRegistry::load(&manifest_path, install_root)?;
 
-    println!("[AutoLoader] bitshit Auto-Loader v{}", registry.manifest.version);
+    println!("[AutoLoader] BitShit Auto-Loader v{}", registry.manifest.version);
     println!("[AutoLoader] Components: {}", registry.manifest.components.len());
 
     let rt = tokio::runtime::Runtime::new()?;
